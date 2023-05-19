2ad5cd3dd6aa13d974982370a99ea87f72b917d3
Nickolas <anickolas00@yahoo.com>
2023-05-19 17:38:47 -0500

    End of section 1

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Activity = Domain.Activity;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Activities represents the table name
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
